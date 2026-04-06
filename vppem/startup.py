13d2dce795f03c7d8822faf71fbf62d819a956d8
Charles Titus <ctitus@bnl.gov>
2026-04-06 13:41:46 -0400

    Dummy startup file for now

--- vppem/startup.py
+++ vppem/startup.py
@@ -1,8 +1,1 @@
-from nbs_bl.detectors import (
-    list_detectors,
-    activate_detector,
-    deactivate_detector,
-    activate_detector_set,
-)
-from nbs_bl.motors import list_motors
-from nbs_bl.beamline import GLOBAL_BEAMLINE as bl
+print("Nothing to do here")
