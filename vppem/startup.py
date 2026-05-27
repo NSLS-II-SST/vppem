1ac93928685ddce0c227fdbdd60db1f95483343b
Charles Titus <ctitus@bnl.gov>
2026-05-27 13:55:32 -0400

    Turning off Keithley SMU components that are not used

--- vppem/startup.py
+++ vppem/startup.py
@@ -1,1 +1,6 @@
-print("Nothing to do here")
+from nbs_bl.configuration import GLOBAL_BEAMLINE as bl
+
+if 'sampleVoltage' in bl.devices:
+    bl.devices['sampleVoltage'].VMeas.kind = "omitted"
+    bl.devices['sampleVoltage'].IMeas.kind = "omitted"
+    bl.devices['sampleVoltage'].ISource.kind = "config"
